af0cb0d5d191df0fa06105dbab23b94b15673470
Newton-A-H <nhinga@byupathway.edu>
2025-07-10 16:28:44 +0300

    temples

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const menuButton = document.querySelector("#menu");
+const navMenu = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+    menuButton.textContent = navMenu.classList.contains("open") ? "x" : "☰"
+});
